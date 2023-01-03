19b430df9fd2bc2d4b5cbdfd25dba0c6229b2f9c
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2023-01-03 18:12:36 +0200

    powershell update

--- Powershell/harjoituksia/harjoitus4.ps1
+++ Powershell/harjoituksia/harjoitus4.ps1
@@ -24,3 +24,35 @@ Handles  NPM(K)    PM(K)      WS(K)     CPU(s)     Id  SI ProcessName
     488      17     4600      20328      38,27   2860   1 ctfmon
 
 
+# pipeline object 
+# pipelien tarkoittaa putki tai tommoinen | 
+
+#komento lyödään powershell, sillo ollaan Pipeline kuvitteelinen putki, joka kuvaa kommenon suoritusta. putken vasemsta sorvutaan objekteja sisään ja putken oikeast pätkästä muokattuja (ei aina). 
+#useampi kommenotja objektejä mm. filteröinti, muokkausta, mittaamisesta, sorttausta, laskemista ja yms. voidaan tuottaa näkymä näillä format komennolla. periaatteessa tapahtuu putken sisällä.
+
+#yleinen Pipeline kommennot on get |set, get ja jne. 
+
+PS C:\WINDOWS\system32> 'BITS','WinRM' | Get-Service
+
+Status   Name               DisplayName
+------   ----               -----------
+Stopped  BITS               BITS (Background Intelligent Transf...
+Stopped  WinRM              Windowsin etähallinta (WS-Management)
+
+
+PS C:\WINDOWS\system32> "BITS","WinRM" | Get-Member
+
+
+   TypeName: System.String
+
+Name             MemberType            Definition
+----             ----------            ----------
+Clone            Method                System.Object Clone(), System.Object ICloneable.Clone()
+CompareTo        Method                int CompareTo(System.Object value), int CompareTo(string strB), int IComparab...
+Contains         Method                bool Contains(string value)
+CopyTo           Method                void CopyTo(int sourceIndex, char[] destination, int destinationIndex, int co...
+EndsWith         Method                bool EndsWith(string value), bool EndsWith(string value, System.StringCompari...
+Equals           Method                bool Equals(System.Object obj), bool Equals(string value), bool Equals(string...
+GetEnumerator    Method                System.CharEnumerator GetEnumerator(), System.Collections.IEnumerator IEnumer...
+GetHashCode      Method                int GetHashCode()
+GetType          Method                type GetType()
