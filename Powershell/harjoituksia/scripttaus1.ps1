4648c3e4abfb3a0471a5c7a09c2707b8934f6284
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2023-01-03 20:17:39 +0200

    powershell update

---
+++ Powershell/harjoituksia/scripttaus1.ps1
@@ -0,0 +1,21 @@
+#harjoitus  pieni scriptaus, usein  scripttaus editointi tapahtuu powershell ISE:ssä, että siellä testataan sen scriptausta ja  antaa vaihtoehtoisia  kommentoja
+
+# kun varsinainen scriptaus on tehty niin saatettaan  pyörittää sitä varsinaisessa admin powershell tai  ihan tavallinen powershell komentorivi
+
+#toistetaan/suoritettaan jokin scripti olemassa
+#vähä kuin  koodaukseski, kun on valmis koodi niin saatettaan ensimmäisenä toistaa mikä koodi kyseessä ja se tiedoston nimi esim. python script.py
+PS C:\Users\zhao-\Documents> .\timezone.ps1
+
+
+Id                         : FLE Standard Time
+DisplayName                : (UTC+02:00) Helsinki, Kiev, Riika, Tallinna, Sofia, Vilna
+StandardName               : Suomen normaaliaika
+DaylightName               : Suomen kesäaika
+BaseUtcOffset              : 02:00:00
+SupportsDaylightSavingTime : True
+
+
+######### scripti malli 
+
+
+
