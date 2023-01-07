9790051bd4a977eaa18771366f749d28b10433ce
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2023-01-07 13:43:34 +0200

    powershell  cli ja yms update

--- Powershell/harjoituksia/harjoitus2_cli.ps1
+++ Powershell/harjoituksia/harjoitus2_cli.ps1
@@ -2,13 +2,36 @@
 toistetaan scriptausta tai muuta vastaava
 
 eli tuttu echo "moi"  niin linux, powershell ja cmd:ssä toistaa tervehdyksen eli moi
-myös koskien kun lukaistaan jokin txt iedosto cat file.txt & ja jne.
+myös koskien kun lukaistaan jokin tietty tiedosto kuten txt, tai scriptaus tai koodipätkät cat file.txt & ja jne. mutta jos tämmöiste csv, word tai muu raportti tiedosto niin tulostuu hirveä erikoismerkkejä, 
+
+josta kukaan ei ymmärrä esim (ò§"Ô!*)È♥?H½æfˆ)£Ž“7
+Ì►£gcj¨›)♂Nc`c\pW►Çä²1Nˆ¡qL▲Š☺ó7N♀òwò7‼"BE↑Á>º‰R¢’¨'6◄ÝÄ6b☼1AÌ►‰£$þFJlºg2ÝŸŽ∟:\sØ3»à[ ‡Ç
+ÆÄ­òN"ÖÄoRBºPúðÂXgS(ÔÔ9¶p8-ÊÝ>dÊmŸ;°ù@ÃµGZŽ♦÷NWL[♠wÛvk{♀♥Tu°NZäWú☼∟™Þ=Pç÷×
+ìž) - vähä kuin salattu tiedosto, ettei voida purkaa sisäistä dataa
 
 C:\Users\Nimi>echo "23"
 "23"
 
 C:\Users\Nimi>echo adsf
 adsf
+
+PS C:\> $PI = 3.14
+PS C:\> Write-Host "The value of `$PI is $PI"
+The value of $PI is 3.14
+
+PS C:\WINDOWS\system32> $testi = "missä minun kahvi on?"
+PS C:\WINDOWS\system32> Write-Host  $testi
+missä minun kahvi on?
+
+# esimerkki cat lukaistaan tiedosto, mutta tämä cat tukee cmd:säskin
+PS C:\Users\zhao-\Documents> cat .\GetHardDiskDetails.ps1
+$aryComputers = "loopback", "localhost"
+Set-Variable -Name intDriveType -Value 3 -Option constant
+foreach ($strComputer in $aryComputers)
+
+ {"Hard drives on: " + $strComputer
+ Get-WmiObject -Class win32_logicaldisk -ComputerName $strComputer|
+ Where {$_.drivetype -eq $intDriveType}}
 
 powershell ympäristössä echo ja write-host ovat sama asia, eli toistettaan jotakin toimintaja, haettavaa dataa tai muuta
 jos on määritetty jokin objekti tai tekijä x on jotakin nin myös sitäkin voidaan toistaa
@@ -30,5 +53,7 @@ eqwr
 fg
 234
 
-
+PS C:\WINDOWS\system32> $testi = "missä minun kahvi on?"
+PS C:\WINDOWS\system32> Write-Host  $testi
+missä minun kahvi on?
 
--- Powershell/harjoituksia/harjoitus7_ps1_psm1.ps1
+++ Powershell/harjoituksia/harjoitus7_ps1_psm1.ps1
@@ -1,8 +1,15 @@
 # https://learn.microsoft.com/en-us/powershell/scripting/developer/module/understanding-a-windows-powershell-module?view=powershell-7.3
-# tiedosto tyyppi  powershell ps1 ja psm1
+<# tiedosto tyyppi  powershell ps1 ja psm1
+
+PowerShell Module (.psm1) & PowerShell script (.ps1)
+
+psm1 - joka sisältää  minkä tahansa kelvollisen windows powershell koodi. Scriptauksen kehittäjä ja admin valvojat voi käyttää tämän tyyppistä moduulia luodakseen lisää moduulia,  joiden jäsenset sisältävät toimintoja, muuttujia ja muuta.
+Komento moduuli  on yksinkertainen windows powershell komentosarja, jolla on eri laajennuksia,  jonka avulla voi  admin valvoja voi käyttää tuonti-, vientiin- ja hallintatoimintoon.
+
+Lopuksi, komentosarjamoduuli, kuten kaikki muutkin moduulit, joita ei luoda dynaamisesti, on tallennettava kansioon, jonka PowerShell voi kohtuudella löytää. Yleensä tämä on PowerShell-moduulin polulla; mutta tarvittaessa voit kuvata tarkasti, mihin moduulisi on asennettu.
 
 
-
+#>
 
 
 
