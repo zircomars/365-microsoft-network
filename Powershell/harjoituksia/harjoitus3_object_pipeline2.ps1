cd11c43302ea7c603db17eedb246427945226f97
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2023-01-08 20:37:46 +0200

    powershell pipeline

---
+++ Powershell/harjoituksia/harjoitus3_object_pipeline2.ps1
@@ -0,0 +1,17 @@
+# pipeline
+
+# pipeline tarkoittaa powershell:in kommennon putki | ()
+
+<# data välitys pipeline usein ei ole ongelmia , ominaisuuden arvo tai parametrien arvo tai parametrien value 
+
+attribuuttit hyväksyy sen pipeline sen arvon ja  muita erikoisia tapauksia ettei toimi, parametri vastaan sen ominaisuuden nimen perusteella ja sillä perustella tuottaa ongelmia silloin pipeline ei toimi ja kuinka ratkaistaan?  se on rissuaita # 
+
+siinä luodaan custom properties name, jotta putkinimen toiselle puolelle eli datan välittäminen toiselle, ja komennossa se menee 
+
+aina, kun välitettään pipeline dataa piilotettu prosessi prosessing piding, sitä ei voi valita koska se on sisään rakennettu parametrien ominaisuus 
+ByValue - passing data 
+
+#>
+
+# exmaple: Command-1 | Command-2 | Command-3
+
