3019d52b56de7df77e7935706a5cf5aabed6f273
zircomars <61022230+zircomars@users.noreply.github.com>
2024-01-11 13:37:01 +0200

    Create sendMailtype1.ps1

---
+++ Powershell/harjoituksia/sendMailtype1.ps1
@@ -0,0 +1,49 @@
+# powershell script send email to user by go Microsoft azure app registration "Microsoft Graph"
+# set app registartion (clientID, cert/sec value, tenant ID)
+$clientID = "<clientID>"
+$Clientsecret = "<sertficiateSecretID>"
+$tenantID = "<tenantID>"
+
+# sender mail , it has to be real one or maybe owner
+$MailSender = "admin@domain.com"
+
+#Connect to GRAPH API
+$tokenBody = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $tokenBody
+$headers = @{
+    "Authorization" = "Bearer $($tokenResponse.access_token)"
+    "Content-type"  = "application/json"
+}
+
+#Send Mail, torecipient (To the recipient)
+$URLsend = "https://graph.microsoft.com/v1.0/users/$MailSender/sendMail"
+$BodyJsonsend = @"
+                    {
+                        "message": {
+                          "subject": "Hello World from Microsoft Graph API",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "This Mail is sent via Microsoft <br>
+                            GRAPH <br>
+                            API<br>
+                            
+                            "
+                          },
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "user1@domain.com"
+                              }
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+
+Invoke-RestMethod -Method POST -Uri $URLsend -Headers $headers -Body $BodyJsonsend
