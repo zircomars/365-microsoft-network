7519e96dbe595a15f1bab7cb69d119fc83eabd06
zircomars <61022230+zircomars@users.noreply.github.com>
2024-02-15 14:41:30 +0200

    Update Get-AzureADUser-nameAndExport.ps1

--- Powershell/harjoituksia/Get-AzureADUser-nameAndExport.ps1
+++ Powershell/harjoituksia/Get-AzureADUser-nameAndExport.ps1
@@ -12,12 +12,19 @@ $Get-AzureADUser | Select-Object -Property userprincipalname, displayname, depar
 UserPrincipalName                                               DisplayName                  Department
 -----------------                                               -----------                  ----------
 
+# but if want to get ALL users then must be add '-all $true' parameters front of the Get-AzureADUser, then it will give export out over 100 users (if got that so much)
+
+$Get-AzureADUser -All $true | Select-Object -Property userprincipalname, displayname, department
+UserPrincipalName                                                         DisplayName                  Department
+-----------------                                                         -----------                  ----------
+
 ###################
 # another example if want to export to csv file (excle sheet table)
 
 $Get-AzureADUser | select userprincipalname, displayname, department  | Export-CSV C:\Users\Name\Path\export-nameList.csv -NoTypeInformation -NoClobber
 
-# another example
+# another example as get -All users
+Get-AzureADUser -All $true | Select-Object -Property userprincipalname, displayname, department | Export-CSV C:\Users\Name\Path\export-nameList.csv -NoTypeInformation -NoClobber
 
 
 
