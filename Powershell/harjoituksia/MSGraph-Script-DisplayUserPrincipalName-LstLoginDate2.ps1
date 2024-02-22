ebe875ea68b1f313ccb36015db5f54180aa2878c
zircomars <61022230+zircomars@users.noreply.github.com>
2024-02-22 15:07:45 +0200

    Update MSGraph-Script-DisplayUserPrincipalName-LstLoginDate2.ps1

--- Powershell/harjoituksia/MSGraph-Script-DisplayUserPrincipalName-LstLoginDate2.ps1
+++ Powershell/harjoituksia/MSGraph-Script-DisplayUserPrincipalName-LstLoginDate2.ps1
@@ -29,3 +29,33 @@ UserPrincipalName                  CompanyName     LastLoginDate
 -----------------                  -----------     -------------
 Mike-John.Doe@domain.fi      Company2              13/02/2024 8.59.00
 Mike.Smith@domain.fi       Company1                21/02/2024 5.55.00
+
+###############################
+# another example export those datat to csv file, like show it to colleague or work partner
+
+$Properties = @(
+    'UserPrincipalName', 'CompanyName', 'SignInActivity'  
+)
+
+# Get all users along with the properties
+$AllUsers = Get-MgUser -All -Property $Properties
+
+# Perform Search
+$FilteredUsers = $AllUsers | Where-Object { $_.UserPrincipalName -like "Mike*" }
+
+# Add LastLoginDate property
+$FilteredUsers | ForEach-Object {
+    $LastLoginDate = $_.SignInActivity.LastSignInDateTime         
+    $_ | Add-Member -MemberType NoteProperty -Name LastLoginDate -Value $LastLoginDate
+}
+
+# Export UserPrincipalName, CompanyName and LastLoginDate properties to a CSV file
+$FilteredUsers | Select-Object UserPrincipalName,CompanyName,LastLoginDate | Export-Csv -Path C:\path\to\yourfile.csv -NoTypeInformation
+
+
+
+
+
+
+
+
