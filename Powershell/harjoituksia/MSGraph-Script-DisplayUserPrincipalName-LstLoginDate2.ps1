f1e466633ae4bd0b26b5d696bd97beb2509a5d13
zircomars <61022230+zircomars@users.noreply.github.com>
2024-02-22 14:22:50 +0200

    Create MSGraph-Script-DisplayUserPrincipalName-LstLoginDate2.ps1

---
+++ Powershell/harjoituksia/MSGraph-Script-DisplayUserPrincipalName-LstLoginDate2.ps1
@@ -0,0 +1,31 @@
+# same as another script (/MSGraph-Script-DisplayUserPrincipalName-LstLoginDate1.ps1)
+# but this add just companyName, like if at admin center got guest or external users, will be added to like a small information
+
+$Properties = @(
+    'UserPrincipalName', 'CompanyName', 'SignInActivity'  
+)
+
+# Get all users along with the properties
+$AllUsers = Get-MgUser -All -Property $Properties
+
+# Perform Search
+$FilteredUsers = $AllUsers | Where-Object { $_.UserPrincipalName -like "Mike*" }
+
+# Add LastLoginDate property
+$FilteredUsers | ForEach-Object {
+    $LastLoginDate = $_.SignInActivity.LastSignInDateTime         
+    $_ | Add-Member -MemberType NoteProperty -Name LastLoginDate -Value $LastLoginDate
+}
+
+# Display UserPrincipalName, CompanyName and LastLoginDate properties
+$FilteredUsers | Format-Table UserPrincipalName,CompanyName,LastLoginDate
+
+
+
+######terminal display example and saved script
+S C:\Users\ZT> .\MSGraph_getUser_lastLoginDate2.ps1
+
+UserPrincipalName                  CompanyName     LastLoginDate
+-----------------                  -----------     -------------
+Mike-John.Doe@domain.fi      Company2              13/02/2024 8.59.00
+Mike.Smith@domain.fi       Company1                21/02/2024 5.55.00
