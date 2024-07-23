62145d9eb162a09b03b2877d3b25a089dafb049b
ZT <61022230+zircomars@users.noreply.github.com>
2024-07-23 20:51:30 +0300

    Update MSGraph-AppRegistration-getUserValue1.ps1

--- Powershell/harjoituksia/MSGraph-AppRegistration-getUserValue1.ps1
+++ Powershell/harjoituksia/MSGraph-AppRegistration-getUserValue1.ps1
@@ -61,8 +61,29 @@ $graph = "https://graph.microsoft.com/v1.0/users"
 # the authorization header which contain the access token, keyword is "bearer" and the value inside the token which when receive.
 # this line variable, get line 57 all the users from this endpoint by using this access token. And this variable will get something interesting and in response, and final will display the variable data out. Final sentence will show and give a filtered the value and what user as get the informations data including user's name, fullname, email domain name (first.last@domain.com) and blah blah and read more able like to convert to json format
 $api = Invoke-RestMethod -Headers @{Authorization = "Bearer $($request.access_token)"} -uri $graph -Method Get
-#  optional, but readable; $api.value | select userPrincipalName, accountEnabled | ConvertTo-json
-$api.value | select userPrincipalName, accountEnabled | ConvertTo-json
+# optional, but readable; $api.value | select userPrincipalName, accountEnabled | ConvertTo-json
+
+##############
+# few output types and how it result looks like, and good practices
+
+## example 1 (two types output one sit like json and other horizontal line, and get first or lasts names with some value from the organization)
+# powershell show api.the value | pipeline select-object like show 10 names if organization got over 100 people then -Property like sort "-First" or "-Last" peoples from organization and filtered show as individual like user properties | final readable like "ConvertTo-json" or "Fortmat-Table"
+# $api.value | Select-Object -First 10 -Property businessPhones, displayName, mobilePhone, userPrincipalName, id | Format-Table
+# $api.value | Select-Object -Last 10 -Property businessPhones, displayName, mobilePhone, userPrincipalName, id | ConvertTo-json
+
+
+#### example 2 , same as example 1, but display 10 names out of the organization without filtered properties | Format-Table read able type or use "ConvertTo-json" is also fine
+#$api.value | Select-Object -First 10 | Format-Table
+
+
+#### example 3, same as example 1 and 2, just output show and receive all names out of the organization
+#$api.value | Format-Table
+
+
+### example 4 , this was the original by following the video tutorial. here "select" as chosen properties item what we want to see and readable now using "convertTo-json"
+#$api.value | select userPrincipalName, accountEnabled | convertTo-json
+
+$api.value | Select-Object -Last 10 -Property businessPhones, displayName, mobilePhone, userPrincipalName, id | Format-Table
 
 
 
